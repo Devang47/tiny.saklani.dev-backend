883b186844558193ea1f1d9ec3d69b093d516c79
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2022-03-07 19:25:07 +0530

    feat: testing endpoint created

--- src/routes/Shorten.js
+++ src/routes/Shorten.js
@@ -4,5 +4,10 @@ import { createShortUrl, getFullURL } from '../controllers/Link.js';
 const router = express.Router();
 router.route('/create').post(createShortUrl);
 router.route('/get').post(getFullURL);
+router.route('/temp').post((req, res) => {
+    console.log('object');
+    res.send('HELLO WORLD')
+});
+
 
 export default router;

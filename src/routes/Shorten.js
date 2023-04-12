ba088fa8657e9c0d240b6328abb644a311cb4cca
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2023-04-12 12:36:17 +0530

    updates

--- src/routes/Shorten.js
+++ src/routes/Shorten.js
@@ -4,9 +4,8 @@ import { createShortUrl, getFullURL } from '../controllers/Link.js';
 const router = express.Router();
 router.route('/create').post(createShortUrl);
 router.route('/get').post(getFullURL);
-router.route('/temp').post((req, res) => {
-    console.log('object');
-    res.send('HELLO WORLD')
+router.route('/ping').all((req, res) => {
+    res.send('Alright, it\'s working.')
 });
 
 
